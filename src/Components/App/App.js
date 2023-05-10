a56d99488fee7c44c8fb22cf28d76057dad8ca2a
Scott Burton <scoburto@gmail.com>
2023-05-09 20:50:49 -0400

    added other component folders and css/js files

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,25 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import SearchBar from '../SearchBar.js';
+import SearchResults from '../SearchResults.js';
+import Playlist from '../Playlist.js';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  function render() {
+    return (
+      <div>
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+        <div className="App">
+          <!-- Add a SearchBar component -->
+          <div className ="App-playlist">
+              <!-- Add a SearchResults component -->
+              <!-- Add a Playlist component -->
+          </div>
+        </div>
+      </div>
+      )
+    }
 }
 
 export default App;
