7b1905635f303d00d75275ae8063015715e7dc96
Scott Burton <scoburto@gmail.com>
2023-05-12 09:26:52 -0400

    about back at the same place as before?

---
+++ src/Components/Track/Track.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+export class Track extends React.Component {
+    render() {
+        return (
+            <div className="Track">
+            <div className="Track-information">
+                <h3>{'songName'}</h3>
+                <p>{'artist'} | {'album'}</p>
+            </div>
+            {this.renderAction}
+            </div>
+        )
+    }
+    renderAction() {
+        return (this.props.isRemoval ? <button className="Track-action">-</button> : <button className="Track-action">+</button>)
+    }
+}
+
+<div class="Track">
+  <div class="Track-information">
+    <h3><!-- track name will go here --></h3>
+    <p><!-- track artist will go here--> | <!-- track album will go here --></p>
+  </div>
+  <button class="Track-action"><!-- + or - will go here --></button>
+</div>
