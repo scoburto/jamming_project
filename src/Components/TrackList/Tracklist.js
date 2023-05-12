27fc00225a3271634a0d5367da22be9eb22a6ff4
Scott Burton <scoburto@gmail.com>
2023-05-11 21:05:46 -0400

    cleaning up repo

---
+++ src/Components/TrackList/Tracklist.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './TrackList.css';
+import { Track } from '../Track/Track.js';
+
+export class TrackList extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div className='TrackList'>
+                {this.props.tracks.map( track => <Track track={track} key={track.id} />  )}
+            </div>
+        )
+    }
+}
