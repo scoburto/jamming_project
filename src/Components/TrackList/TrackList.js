aac3c9a478d56ccf469e331bbf63e932a035d9f0
Scott Burton <scoburto@gmail.com>
2023-05-19 11:37:39 -0400

    app working!! now need to move on to "section4: pass down playlist to TrackList

--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './TrackList.css';
+import { Track } from '../Track/Track';
 
 export class TrackList extends React.Component {
     render() {
         return (
             <div className="TrackList">
-                {/* < -- map method that renders a set of Track components  --> */}
+                {this.props.tracks.map(track => <Track key={track.id} track={track}/>)}
             </div>
         )
     }
