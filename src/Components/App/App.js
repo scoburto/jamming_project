370a5fee136bd19e901a6b8ad7232c26c65248f2
Scott Burton <scoburto@gmail.com>
2023-06-02 09:55:36 -0400

    fixed issue with npm not calling start script by fiddling with the packagejson and then also added playlist name update methods and attrs to components

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -19,6 +19,7 @@ class App extends React.Component {
     };
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
+    this.updatePlaylistName = this.updatePlaylistName.bind(this);
   }
   render() {
     return (<div>
@@ -29,7 +30,8 @@ class App extends React.Component {
                   <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack} />
                   <Playlist playlistName={this.state.playlistName} 
                             playlistTracks={this.state.playlistTracks} 
-                            onRemove={this.removeTrack} />
+                            onRemove={this.removeTrack} 
+                            onNameChange={this.updatePlaylistName} />
                 </div>
               </div>
             </div>)
@@ -48,6 +50,9 @@ class App extends React.Component {
   removeTrack(track) {
     let newPlaylist = this.state.playlistTracks.filter(playTrack => playTrack.id != track.id);
     this.setState({playlistTracks: newPlaylist});
+  }
+  updatePlaylistName(newName) {
+    this.setState({playlistName: newName});
   }
 }
 
--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -3,13 +3,22 @@ import './Playlist.css';
 import { TrackList } from '../TrackList/TrackList';
 
 export class Playlist extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleNameChange = this.handleNameChange.bind(this);
+    }
     render() {
         return (
             <div className="Playlist">
-                <input defaultValue={'New Playlist'} />
-                <TrackList tracks={this.props.playlistTracks} onRemove={this.props.onRemove} isRemoval={true}/>
+                <input defaultValue={'New Playlist'} onChange={this.handleNameChange} />
+                <TrackList tracks={this.props.playlistTracks} 
+                           onRemove={this.props.onRemove} 
+                           isRemoval={true}/>
                 <button className="Playlist-save">SAVE TO SPOTIFY</button>
             </div>
         )
+    }
+    handleNameChange(event) {
+        this.props.onNameChange(event.target.value);
     }
 }
